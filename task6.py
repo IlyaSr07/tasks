cb07c4fa387cba517733015b222a9acd7fd9ca9e
Ilya <fanprofun@gmail.com>
2022-09-25 11:14:21 +0300

    finished

--- task6.py
+++ task6.py
@@ -14,16 +14,25 @@ class CityMap:
             self.map.append(f)
         v = randint(0, (length-1))
         if randint(1, 2) == 1: #h roads
-            for i in range(width):
+            for i1 in range(width):
                 self.map[v][h] = '*'
                 h = h+1
         else:
             if v == 0:
                 v=v+1
-            for i in range(width):
+            for i2 in range(width):
                 self.map[v][h] = '*'
                 self.map[v-1][h] = '*'
                 h = h+1
+        h = randint(0, (length-1))
+        if h<2:
+          h = 2
+        print(v, h)
+        for i3 in range(length):
+          self.map[v][h] = '*'
+          self.map[v][h-1] = '*'
+          self.map[v][h-2] = '*'
+          v = v+1
 
     def showmap(self):
         for part in self.map:
